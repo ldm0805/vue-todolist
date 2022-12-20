d1bbf326ec25fcd27d69a844c1050c7e71e38e17
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-20 15:06:13 +0100

    Milestone 3 e bonus completi

--- js/script.js
+++ js/script.js
@@ -9,38 +9,26 @@ createApp({
                 {
                     text:'Fare la lista dei regali di natale;',
                     done: false,
-                    edit: false
-
                 },
                 {
                     text:'Portare a spasso il cane;',
                     done: false,
-                    edit: false
-
                 },
                 {
                     text:'Scegliere quale serie tv seguire;',
                     done: true,
-                    edit: false
-
                 },
                 {
                     text:'Passare l\'aspirapolvere;',
                     done: false,
-                    edit: false
-
                 },
                 {
                     text:'Seguire lo snack pomeridiano;',
                     done: true,
-                    edit: false
-
                 },
                 {
                     text:'Fare l\'esercizio di boolean;',
                     done: false,
-                    edit: false
-
                 },
             ]
         }
@@ -67,16 +55,5 @@ createApp({
                 task.done = true;
             }
         },
-        showEdit(index){
-            let task = this.todolist[index];
-
-            if(task.edit == true){
-                task.edit = false;
-            }
-            else{
-                task.edit = true;
-            }
-        },
-
     }
 }).mount('#app');
