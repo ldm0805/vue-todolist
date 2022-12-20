8f94d84bc4c61bbc2542b88e5eaa9eb8da30102c
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-20 14:48:46 +0100

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -9,31 +9,37 @@ createApp({
                 {
                     text:'Fare la lista dei regali di natale;',
                     done: false,
+                    edit: false
 
                 },
                 {
                     text:'Portare a spasso il cane;',
                     done: false,
+                    edit: false
 
                 },
                 {
                     text:'Scegliere quale serie tv seguire;',
                     done: true,
+                    edit: false
 
                 },
                 {
                     text:'Passare l\'aspirapolvere;',
                     done: false,
+                    edit: false
 
                 },
                 {
                     text:'Seguire lo snack pomeridiano;',
                     done: true,
+                    edit: false
 
                 },
                 {
                     text:'Fare l\'esercizio di boolean;',
                     done: false,
+                    edit: false
 
                 },
             ]
@@ -41,6 +47,17 @@ createApp({
     },
 
     methods:{
+        addTask(){
+            let object = {
+                text: this.newTask,
+                done: false,
+            }
+            this.todolist.push(object);
+            this.newTask = '';
+        },
+        removeTask(index){
+            this.todolist.splice(index, 1);
+        },
         checkTask(index){
             let task = this.todolist[index];
             if(task.done == true){
@@ -49,7 +66,17 @@ createApp({
             else{
                 task.done = true;
             }
-        }
+        },
+        showEdit(index){
+            let task = this.todolist[index];
+
+            if(task.edit == true){
+                task.edit = false;
+            }
+            else{
+                task.edit = true;
+            }
+        },
 
     }
 }).mount('#app');
